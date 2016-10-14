9326b8174814edec33e59361bb13a4a325571fb7
rong_yu <rong_yu@sumilux.cn>
2016-10-14 18:40:51 +0800

      双向数据绑定

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,6 +9,57 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('View1Ctrl', ['$scope',
+        function($scope) {
+            $scope.userInfo={
+              email:"test@fp.com",
+              password:"222222",
+            };
+            $scope.getFormDate=function () {
+                console.log ($scope.userInfo);
+            };
+            $scope.setFormDate=function () {
+                $scope.userInfo={
+                    email:"admin@fp.com",
+                    password:"abc123",
+                }
+            };
+            $scope.resetFormDate=function () {
+                $scope.userInfo={
+                    email:"test@fp.com",
+                    password:"222222",
+                }
+            };
 
-}]);
+            //ng-class
+            $scope.color = "danger";
+            $scope.setGreen = function(){
+                $scope.color = "success";
+            };
+
+            $scope.isError = false;
+            $scope.isWarning = false;
+            $scope.showError = function () {
+                $scope.messageText = 'This is an error!';
+                $scope.isError = true;
+                $scope.isWarning = false;
+            };
+            $scope.showWarning = function () {
+                $scope.messageText = 'Just a warning.Please carry on.';
+                $scope.isWarning = true;
+                $scope.isError = false;
+            };
+
+            //ng-show ng-hide
+            $scope.menuState = {show:false};
+            $scope.toggleMenu = function(){
+                $scope.menuState.show = !$scope.menuState.show;
+            };
+
+
+            $scope.greeting = {
+                text: 'Hello'
+            };
+            $scope.pageClass="hello";
+        }
+]);
--- app/view2/view2.js
+++ app/view2/view2.js
@@ -9,6 +9,17 @@ angular.module('myApp.view2', ['ngRoute'])
   });
 }])
 
-.controller('View2Ctrl', [function() {
-
+.controller('View2Ctrl', ['$scope',
+      function($scope) {
+          $scope.books = [{
+            title: "《Ext江湖》",
+            author: "大漠穷秋"
+          }, {
+            title: "《ActionScript游戏设计基础（第二版）》",
+            author: "大漠穷秋"
+          }, {
+            title: "《用AngularJS开发下一代WEB应用》",
+            author: "大漠穷秋"
+          }];
+          $scope.pageClass="list";
 }]);
